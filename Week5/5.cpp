3348b8529d0353a8b231eb30b51467b8634f1afd
Sumurth Dixit <sumurth17dixit05@gmail.com>
2022-07-31 17:56:32 +0530

    Create 5.cpp

---
+++ Week5/5.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minDistance(string s, string t) {
+        int n = s.size(), m = t.size(); 
+        vector<int> prev(m + 1), cur(m + 1); 
+        
+        for(int j = 0; j <= m; j++) {
+            prev[j] = j; 
+        }
+        for(int i = 1; i <= n; i++) {
+            cur[0] = i; 
+            
+            for(int j = 1; j <= m; j++) {
+                if(s[i - 1] == t[j - 1]) 
+                    cur[j] = prev[j - 1]; 
+                else {
+                    cur[j] = 1 + min({prev[j], cur[j - 1], prev[j - 1]}); 
+                }
+            }
+            prev = cur; 
+        }
+        return prev[m]; 
+    }
+};
