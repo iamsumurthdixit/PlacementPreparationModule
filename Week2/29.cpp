6cf3c8590e32e56271d3b1162851488a76e4dcef
Sumurth Dixit <sumurth17dixit05@gmail.com>
2022-07-31 17:18:16 +0530

    Create 29.cpp

---
+++ Week2/29.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> pq; 
+        for(int n : nums) pq.push(n); 
+        k--; 
+        while(k--) {
+            pq.pop(); 
+        }
+        return pq.top(); 
+    }
+};
